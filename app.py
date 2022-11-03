2b263a19bc76a2c94b6fdf756c7bf64bc1cacf50
rdamaid1 <116780824+rdamaid1@users.noreply.github.com>
2022-11-03 10:20:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,15 +40,17 @@ def predict():
     ligand_protein = list0 + target
 
     input0 = pd.DataFrame(ligand_protein).transpose()
+    
+    put_text(input0)
 
-    prediction = model.predict([[input0]])
-    output = round(prediction[0], 10)
+#     prediction = model.predict([[input0]])
+#     output = round(prediction[0], 10)
 
-    if output < 0:
-        put_text("Sorry, the ligand protein interaction cannot be predicted")
+#     if output < 0:
+#         put_text("Sorry, the ligand protein interaction cannot be predicted")
 
-    else:
-        put_text('Your ligand protein interaction prediction score is:', output)
+#     else:
+#         put_text('Your ligand protein interaction prediction score is:', output)
         
     # Year = input("Enter the Model Year：", type=NUMBER)
     # Year = 2021 - Year
